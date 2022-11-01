45a6aeb290fdd7f9cd53edb1870954cfa3a2470e
Mang RY <99274133+blackshadowzz@users.noreply.github.com>
2022-11-02 01:43:23 +0700

    Update Form1.Designer.cs

--- BusMS/Form1.Designer.cs
+++ BusMS/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(553, 272);
+            this.button2.Location = new System.Drawing.Point(553, 555);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(166, 167);
             this.button2.TabIndex = 1;
